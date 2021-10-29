55df29eaf032759da126dc488e28638711f8b58f
Charith Lakshitha <charithlakshitha@outlook.com>
2021-10-29 19:35:50 +0530

    Add project files.

---
+++ BattleshipLiteLibrary/Models/PlayerInfoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLiteLibrary.Models
+{
+    public class PlayerInfoModel
+    {
+        public string UserName { get; set; }
+        public List<GridSpotModel> SpotModels { get; set; }
+        public List<GridSpotModel> ShotGrid { get; set; }
+
+
+    }
+}
